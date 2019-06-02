b7b0274a48ea45c52b288927a22886cdca104bf3
Anastasia <atamantseva2016@ya.ru>
2019-06-02 21:56:48 +0300

    Меню с помощию цикла

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,17 +1,55 @@
+   
+
 'use strict'
 
-
-const showMenu = document.querySelectorAll('.menu__item')	
+const showMenu = document.querySelectorAll('.menu__item')
 let menu = Array.from(showMenu)
 
+const showMenuSub = document.querySelectorAll('.menu_sub');
+let menuSub = Array.from(showMenuSub);
+
+
+
 for (let index of menu){
-	if (index.querySelectorAll('.menu_sub')){
+	if (index.querySelector('.menu_sub')){
 	index.querySelector('.menu__link').onclick = function (){
-	index.closest('.menu').className = 'menu active';	
-	index.querySelector('.menu_sub').className = 'menu menu_sub menu_active'
-    return false;
+    for (let item of showMenuSub){
+    	item.className = 'menu menu_sub '
+    }
+      if (index.querySelector('.menu_sub').className = 'menu menu_sub menu_active')
+      return false;
+	}
 }
 
 }
 
-}
+
+// 'use strict'
+
+
+
+
+
+// const showMenu = document.querySelectorAll('.menu__item')	
+
+// let menu = Array.from(showMenu)
+
+
+
+// for (let index of menu){
+
+// 	if (index.querySelectorAll('.menu_sub')){
+
+// 	index.querySelector('.menu__link').onclick = function (){
+
+// 	index.closest('.menu').className = 'menu active';	
+
+// 	index.querySelector('.menu_sub').className = 'menu menu_sub menu_active'
+
+//     return false;
+
+// }
+
+// }
+
+// }
